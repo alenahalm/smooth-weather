3508367b5fed4387c09aa7e66fa1471d7c13c236
alenahalm <75882124+alenahalm@users.noreply.github.com>
2022-09-25 12:01:44 +0800

    Add files via upload

---
+++ Main.kt
@@ -0,0 +1,22 @@
+import java.util.*
+
+fun smooth(index:Int, arr: Array<Float>):Float {
+    return if (index > 0 && index < arr.size - 1) {
+        (arr[index-1]+arr[index]+arr[index+1]) / 3
+    }
+    else {
+        arr[index]
+    }
+}
+
+fun main() {
+
+    val sc = Scanner(System.`in`)
+    val n = readLine()!!.split(" ")[0].toInt()
+    val line = readLine()!!.split(" ")
+    val arr = Array(n){i -> line[i].toFloat()}
+
+    val smooth_arr = Array(arr.size){num -> smooth(num, arr)}
+    println(smooth_arr.contentToString())
+
+}
